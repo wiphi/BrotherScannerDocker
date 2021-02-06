b64f95caa8ac156198d7a1f0e0f63a00eb67889f
wiphi <wiphi@noreply.github.com>
2021-02-06 19:14:22 +0100

    fix script

--- files/runScanner.sh
+++ files/runScanner.sh
@@ -1,21 +1,23 @@
 SCRIPT_DIR=/opt/brother/scanner/brscan-skey/script
 SCRIPT_TMP_DIR=/tmp/script
-
+echo add brotherscanner user
 # restore scripts, don't overwrite existing (-n)
-cp -v -n $SCRIPT_TMP_DIR/* $SCRIPT_DIR
+#cp -v -n $SCRIPT_TMP_DIR/* $SCRIPT_DIR
 
 adduser brotherscanner --disabled-password --force-badname --gecos ""
 
 chmod -R 777 /home/brotherscanner
-
+echo save environment
 env > /opt/brother/scanner/env.txt
+echo change user and start scanner app
+echo config scanner
 su - brotherscanner -c "brsaneconfig4 -a name=$NAME model=$MODEL ip=$IPADDRESS"
-sleep 1000
+echo start scanner
 su - brotherscanner -c "brscan-skey -t"
-sleep 1000
 su - brotherscanner -c "brscan-skey"
-sleep 1000
+echo skey is running...
 su - brotherscanner -c "brscan-skey -l"
+echo waiting for  your docs...
 while true;
 do
   sleep 1000
